e446a5a6a7cef4e1bf6a0d6ede3d1f958ac3b628
Francisco torres <ftorres767@gmail.com>
2021-09-29 18:47:39 -0300

    componentes productCard, productCards y searchBar(primer PR solucionado)

--- client/src/components/NavBar/SearchBar.jsx
+++ client/src/components/NavBar/SearchBar.jsx
@@ -2,25 +2,19 @@ import  {useState} from 'react'
 
 const searchBar = () => {
 
-    let [state, setState] = useState({
-        inputFunction: ""
-    })
+    let [state, setState] = useState("")
 
-    const onChangeInput = (e) => {
-        setState({
-            ...state,
-            [e.target.name] : e.target.value
-    })
-    }
+    const onChangeInput = (e) => setState(e.target.value);
 
-    const onSubmitFunction = () => {
+    const onSubmitFunction = (e) => {
+     e.preventDefault()
      dispatch(laAccion(state)) // deberia despachar la accion pero todavia no estan hechas
     }
 
     return (
         <div>
             <form onSubmit={onSubmitFunction}>
-                <input type="text" name="inputFunction"  value={state.inputFunction} onChange={onChangeInput} placeholder="buscar..."></input>
+                <input type="text" name="inputFunction"  value={state} onChange={onChangeInput} placeholder="buscar..."></input>
                 <button type="onSubmit"></button>
             </form>
         </div>
