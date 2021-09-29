ed760b873c870acbc04ad85cb386b62f5d1453b1
Francisco torres <ftorres767@gmail.com>
2021-09-29 20:04:07 -0300

    rutas creadas en app.js para mostrar componentes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route } from "react-router-dom";
+import CatalogueView from "./views/CatalogueView/CatalogueView"
 
 function App() {
   return (
     <div className="App">
       <h1>Henry Final Project</h1>
+      <Route exact path="/product" component={CatalogueView}></Route>
+      <Route exact path="/product/:id" component={ProductDetail}></Route>
     </div>
   );
 }
