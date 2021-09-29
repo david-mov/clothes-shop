fd115b490a6c06961983498111818baed7cd468e
Francisco torres <ftorres767@gmail.com>
2021-09-29 00:27:51 -0300

    modelo entidad relacion creado

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,25 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { User } = sequelize.models;
+const { User, Type, Rol, Product, Image, Category } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+User.belongsToMany(Product, {through: "user_product"})
+Product.belongsToMany(User, {through: "user_product"})
+
+Type.hasMany(Product)
+Product.belongsTo(Type)
+
+Product.belongsToMany(Category, {through: "category_product"})
+Category.belongsToMany(Product, {through: "user_product"})
+
+Product.hasMany(Image)
+Image.belongsTo(Product)
+
+Rol.hasMany(User)
+User.belongsTo(Rol)
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
