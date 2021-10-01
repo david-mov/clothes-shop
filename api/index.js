cef8bcc3c442b6a32e0642cabacacee71ecfcacc
Deskmont162525 <79815711+Deskmont162525@users.noreply.github.com>
2021-10-01 17:31:36 -0500

    resto de add

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 const { DB_PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(DB_PORT, () => {
     console.log(`%somos los mejores listening at ${DB_PORT}`); // eslint-disable-line no-console
   });
