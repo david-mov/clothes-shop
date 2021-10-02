5118d9d22650c233ad2c55b61366f6b1b0a4a7a1
David <83718324+david-mov@users.noreply.github.com>
2021-10-01 23:49:20 -0300

    refactor reducer

--- client/src/stateManagement/reducer/index.js
+++ client/src/stateManagement/reducer/index.js
@@ -1,26 +1,8 @@
-import { 
-	GET_ALL_PRODUCTS,
-	GET_ALL_CATEGORIES,
-} from '../../consts/actionConsts';
+import { combineReducer } from 'redux';
+import categoriesReducer from './categoriesReducer.js'
+import productsReducer from './productsReducer.js'
 
-const initialState = {
-	products: [],
-	categories: [],
-};
-
-export default function rootReducer(state = initialState, action) {
-	switch(action.type) {
-		case GET_ALL_PRODUCTS:
-			return ({
-				...state,
-				products: action.payload
-			});
-		case GET_ALL_CATEGORIES:
-			return ({
-				...state,
-				categories: action.payload
-			});
-		default:
-			return ({ ...state });
-	}
-}
+export default combineReducer(
+	categoriesReducer,
+	productsReducer
+)
