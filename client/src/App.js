e8a3882428a88f40270c268c235c201d801e92e5
David <83718324+david-mov@users.noreply.github.com>
2021-10-03 04:06:56 -0300

    commit with conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomeView from './views/HomeView/HomeView';
 import NotFoundView from './views/NotFoundView/NotFoundView';
+<<<<<<< Updated upstream
+=======
+<<<<<<< Updated upstream
+import ProfileView from './views/ProfileView/ProfileView';
+import ProductView from './views/ProductView/ProductView';
+=======
+
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 function App() {
   return (
     <Router>
--- client/src/stateManagement/reducer/index.js
+++ client/src/stateManagement/reducer/index.js
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 import { combineReducer } from 'redux';
+=======
+<<<<<<< Updated upstream
+import { combineReducers } from 'redux';
+>>>>>>> Stashed changes
 import categoriesReducer from './categoriesReducer.js'
 import productsReducer from './productsReducer.js'
 
@@ -6,3 +11,31 @@ export default combineReducer(
 	categoriesReducer,
 	productsReducer
 )
+=======
+import { 
+	GET_ALL_PRODUCTS,
+	GET_ALL_CATEGORIES,
+} from '../../consts/actionConsts';
+
+const initialState = {
+	products: [],
+	categories: [],
+};
+
+export default function rootReducer(state = initialState, action) {
+	switch(action.type) {
+		case GET_ALL_PRODUCTS:
+			return ({
+				...state,
+				products: action.payload
+			});
+		case GET_ALL_CATEGORIES:
+			return ({
+				...state,
+				categories: action.payload
+			});
+		default:
+			return ({ ...state });
+	}
+}
+>>>>>>> Stashed changes
