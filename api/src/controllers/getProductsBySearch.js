fa6f83d53f6276e592481796ecaafbaec55fa41e
Francisco torres <ftorres767@gmail.com>
2021-10-03 14:42:38 -0300

    todos los filtros y busqueda funcionando

--- api/src/controllers/getProductsBySearch.js
+++ api/src/controllers/getProductsBySearch.js
@@ -1,5 +1,12 @@
-const getProductsBySearch = (req, res, next) => {
+const {Product} = require("../db")
 
+const getProductsBySearch = async(req, res, next) => {
+const {name} = req.query
+    const myProducts = await Product.findAll()
+        var filtered = await myProducts.filter((e) => 
+        e.name.toLowerCase().includes(name.toLowerCase())
+        )
+        return res.json(filtered)
 }
 
 module.exports = getProductsBySearch;
--- client/src/stateManagement/reducer/productsReducer.js
+++ client/src/stateManagement/reducer/productsReducer.js
@@ -1,7 +1,9 @@
 import { 
 	GET_ALL_PRODUCTS,
 	GET_CATEGORY,
-	GET_TYPE
+	GET_TYPE,
+	GET_FILTER_BY_PRICE,
+	GET_SEARCH
 } from '../../consts/actionConsts';
 
 const initialState = {
@@ -20,15 +22,39 @@ export default function productsReducer(state = initialState, action) {
 			case GET_CATEGORY:
 			return {
 				...state,
-				products: state.productsCopy.filter((e) => e.categories?.some((sne) => sne.name === action.payload))
+				products: action.payload === "none" ? state.productsCopy : state.productsCopy.filter((e) => e.categories?.some((sne) => sne.name === action.payload))
 			}
 			case GET_TYPE: 
 			console.log("ENTRE EN EL REDUCER DE TYPE")
 			console.log(action.payload)
 			return {
 				...state,
-				products: state.productsCopy.filter((e) => e.type?.name === action.payload)
+				products: action.payload === "none" ? state.productsCopy : state.productsCopy.filter((e) => e.type?.name === action.payload)
 			}
+			case GET_FILTER_BY_PRICE:
+				if(action.payload === "none"){
+					return {
+						...state,
+						products: state.productsCopy
+					}
+				}
+				else if(action.payload === "H"){
+					return {
+						...state,
+						products: state.productsCopy.sort((a, b) => (a.price < b.price) ? 1 : -1)
+					}
+				}else{
+					return {
+						...state,
+						products: state.productsCopy.sort((a, b) => (a.price > b.price) ? 1 : -1)
+					}
+				}
+				case GET_SEARCH: {
+					return {
+						...state,
+						products: action.payload
+					}
+				}
 		default:
 			return ({ ...state });
 	}
