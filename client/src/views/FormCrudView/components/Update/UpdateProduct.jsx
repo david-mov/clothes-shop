e208ff2e28caed8423c4de3dd1458f6cf5ea266a
rodriquialvo <rodriiaguantlab@gmail.com>
2021-10-04 00:39:35 -0300

    CrudUpdate

--- client/src/views/FormCrudView/components/Update/UpdateProduct.jsx
+++ client/src/views/FormCrudView/components/Update/UpdateProduct.jsx
@@ -1,64 +1,122 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState  } from "react";
 import Select from "react-select";
-import { useDispatch, useSelector } from "react-redux";
-import { postAllProducts } from "../../../../stateManagement/actions/postAllProducts";
-import { getAllCategories } from "../../../../stateManagement/actions/getAllCategories";
-import { getAllsizes } from "../../../../stateManagement/actions/getAllsizes";
-import { getAllTypes } from "../../../../stateManagement/actions/getAllTypes";
-import "./Insert.css";
+import { useSelector, useDispatch } from "react-redux";
+import { getAllCategories } from "../../stateManagement/actions/getAllCategories";
+import { getAllsizes } from "../../stateManagement/actions/getAllsizes";
+import { getAllTypes } from "../../stateManagement/actions/getAllTypes";
+import {putProduct} from "../../stateManagement/actions/putProduct"
 
-const validate = (input) => {
-  let errors = {};
-  if (!input.name) {
-    errors.name = "Required field enter a name";
-  } else if (!input.price) {
-    errors.price = "Required field enter a Price";
-  } else if (!input.description) {
-    errors.description = "Required field enter a Description";
-  } else if (!input.stock) {
-    errors.stock = "Required field enter a amount";
-  } else if (!input.color) {
-    errors.color = "Required field enter a color";
-  } else if (!input.valueSize) {
-    errors.valueSize = "Campo requerido ingresa un Rating";
-  }
-  return errors;
-};
 
-const Insert = () => {
+// let obj = {
+//   id:1,
+//   name: "medias",
+//   description: "medias largas",
+//   price: "",
+//   color: "rojo",  
+//   stock: 15,
+//   type_product: 5,
+//   categories: [{id: 1, name: "rodrsdasdsadi"},{id: 2, name: "rodrsdsdi"},{id: 3, name: "rosdsaddri"}],
+//   sizes: [{id: 1, name: "rodri"},{id: 2, name: "rodrigo"}],
+// }
+
+
+
+
+const Update = ({
+  id=1,
+  name="rodri", 
+  price=200,
+  description="sads",
+  color="rojo",
+  stock=1,
+  type_product=2,
+  type = {
+    name: "pantalon"
+  },
+  categories=[{id: 2, name: "futbol"},{id: 5, name: "elegante"},{id: 4, name: "sport"}],
+  sizes = [{id: 1, name: "big"},{id: 2, name: "medium"}],
+  image=""
+  
+}) => {
+
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getAllCategories());
+    dispatch(getAllCategories()); 
     dispatch(getAllsizes());
-    dispatch(getAllTypes());
-    dispatch(postAllProducts());
+    dispatch(getAllTypes()); 
+    // dispatch(putProduct())   
   }, [dispatch]);
-  const [input, setInput] = useState({
-    name: "",
-    price: "",
-    description: "",
-    color: "",
-    stock: "",
-    type_product: 0,
-    categories: [],
-    sizes: [],
-    images: [],
-  });
+
+
+
   
-  let categories = useSelector((state) => state.categoriesReducer.categories);
-  let sizes = useSelector((state) => state.sizesReducer.sizes);
-  let types = useSelector((state) => state.typesReducer.types);
-  const [valueCate, setvalueCate] = useState(null);
-  const [valueSize, setvalueSize] = useState(null);
-  const [valueType, setvalueType] = useState(null);
-  const [errors, setErrors] = useState({});
-  const Options = categories.map((e) => {
+
+
+  // console.log("*********************************************", mapCategories)
+  let categoriess = useSelector((state) => state.categories);
+  let sizess = useSelector((state) => state.sizes);
+  let types = useSelector((state) => state.types);
+  const mapCategories = categories.map(e =>({
+    value: e.id,
+    label: e.name
+  }));
+
+
+  const mapSizes = sizes.map(e =>({
+    value: e.id,
+    label:e.name
+  }));
+
+
+  // const mapTypeFind =  types.map(e => {
+  //   if(e.id === type_product) {
+  //     return e.name
+  //   }    
+  // });
+  // let nameType = "";
+  // mapTypeFind.forEach(element => {
+  //   if(element) {
+  //    nameType = element
+  //   }
+  // });
+  // // console.log("NOMBRE", nameType )
+  
+  
+  const mapType = {value: type_product, label: type.name}
+
+  // console.log("MAPTYPE", mapType)
+
+
+  const [valueCate, setvalueCate] = useState(mapCategories);
+  const [valueSize, setvalueSize] = useState(mapSizes);
+  const [valueType, setvalueType] = useState(mapType);
+
+  const [input, setInput] = useState({
+    name,
+    description,
+    color,
+    image,
+    stock,
+    type_product,
+    categories: valueCate,
+    sizes: valueSize,
+    price
+  })
+
+  // console.log("ESTADOOOOO",valueType)
+
+
+
+  
+  // // setvalueCate(mapCategories)
+  
+  const Options = categoriess.map((e) => {
     return {
       value: e.id,
       label: e.name,
     };
   });
-  const Optionsize = sizes.map((e) => {
+  const Optionsize = sizess.map((e) => {
     return {
       value: e.id,
       label: e.name,
@@ -70,24 +128,38 @@ const Insert = () => {
       label: e.name,
     };
   });
+
+
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const onSelectChangeNew = (valueCate) => {
-    var tipesEnv = "";
+    let tipesEnv = "";
     if (valueCate) {
       tipesEnv = valueCate.map((e) => {
         return e.value;
       });
+     
     }
     setvalueCate(valueCate);
     addcategories(tipesEnv);
   };
+
+  
   const addcategories = (tipesEnv) => {
     setInput({
       ...input,
       categories: tipesEnv,
     });
   };
+  
+
   const onSelectChangeNewSize = (valueSize) => {
-    var tipesEnv = "";
+    let tipesEnv = "";
     if (valueSize) {
       tipesEnv = valueSize.map((e) => {
         return e.value;
@@ -95,73 +167,82 @@ const Insert = () => {
     }
     setvalueSize(valueSize);
     addSizes(tipesEnv);
-    setErrors(
-      validate({
-        ...input,
-       // [valueSize.target.name]: valueSize.target.value,
-      })
-    );
   };
+
   const addSizes = (tipesEnv) => {
     setInput({
       ...input,
       sizes: tipesEnv,
     });
   };
+
   const onSelectChangeNewType = (valueType) => {
+    // let tipesEnv = "";
+    // if (valueType) {
+    //   // tipesEnv = valueType.map((e) => {
+    //   //   return e.value;
+    //   // });
+    //   setvalueType(valueType);
+    //   set
+    // }
+
     setInput({
       ...input,
-      type_product: valueType.value,
+      type_Product: valueType.value,
     });
 
     setvalueType(valueType);
-  };
-
-  const handleChange = (e) => {
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-    setErrors(
-      validate({
-        ...input,
-        [e.target.name]: e.target.value,
-      })
-    );
-  };
-  const cerrarModalInsertar = () => {
-    setInput({ modalInsertar: false });
+    
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    // await postAllProducts(input);
+    // alert("Product has edited correctly");
+      let mapCategorie = input.categories.map(e => e.value);
+      let mapSizes = input.sizes.map(e => e.value);
 
-    dispatch(postAllProducts(input));
-    alert("Product has created correctly");
-    return setInput({
-      name: "",
-      price: "",
-      description: "",
-      color: "",
-      stock: "",
-      type_product: 0,
-      categories: [],
-      sizes: [],
-      images: [],
-    });
+      let obj = {
+        name: input.name,
+        description: input.description,
+        color:input.color,
+        image:input.image,
+        stock:input.stock,
+        type_product:input.type_product,
+        categories: mapCategorie,
+        sizes: mapSizes,
+        price:input.price
+      }
+      
+    
+
+    console.log("OBJ",obj);
+    console.log("INPUT",input)
+    
   };
-console.log("data", categories)
+
+  const cerrarModalInsertar = () => {
+    setInput({ modalInsertar: false });
+  };
+
+  console.log(input)
+
   return (
     <div className="crud_form">
-      <form>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
         <div className="insertar">
           <div>
             <h3 className="h3_insert">Insert Product</h3>
           </div>
           <div className="insert_label">
             <label>Categories</label>
+            <input name = "categories" type= "text" value = {id} readOnly />
             <Select
-              className="selected"
+              name = "categories"
               value={valueCate}
               options={Options}
               isMulti
@@ -171,83 +252,75 @@ console.log("data", categories)
           <div className="insert_label">
             <label>Zise</label>
             <Select
-              className="selected"
               value={valueSize}
               options={Optionsize}
               isMulti
               onChange={(e) => onSelectChangeNewSize(e)}
             />
           </div>
-          {errors.sizes && <p>{errors.sizes}</p>}
           <div className="insert_label">
             <label>Type</label>
             <Select
-              className="selected"
               value={valueType}
               options={OptionType}
               onChange={(e) => onSelectChangeNewType(e)}
             />
           </div>
           <div className="insert_label">
-            <label className="label_Insert">Name</label>
+            <label className="label_Insert">Name:</label>
             <input
               className="form-control"
               name="name"
               type="text"
-              value={input.name}
               onChange={handleChange}
+              value = {input.name}
             />
-            {errors.name && <p>{errors.name}</p>}
-            <label className="label_Insert">Price</label>
+            <label className="label_Insert">Price:</label>
             <input
               className="form-control"
               name="price"
               type="text"
-              value={input.price}
               onChange={handleChange}
+              value={input.price}
             />
-            {errors.price && <p>{errors.price}</p>}
-            <label className="label_Insert">Description</label>
+
+            <label className="label_Insert">Description:</label>
             <input
               className="form-control"
               name="description"
               type="text"
-              value={input.description}
               onChange={handleChange}
+              value = {input.description}
             />
-            {errors.description && <p>{errors.description}</p>}
-            <label className="label_Insert">Stock</label>
+            <label className="label_Insert">Stock:</label>
             <input
               className="form-control"
               name="stock"
               type="number"
-              min="0"
-              onClick={handleChange}
+              min="0" 
+              onChange={handleChange}
+              value = {input.stock}
             />
-            {errors.stock && <p>{errors.stock}</p>}
-            <label className="label_Insert">Color</label>
+            <label className="label_Insert">Color:</label>
             <input
               className="form-control"
               name="color"
               type="text"
-              value={input.color}
               onChange={handleChange}
+              value = {input.color}
             />
-            {errors.color && <p>{errors.color}</p>}
+
+            <label className="label_Insert">Image:</label>
           </div>
           <div className="crud_Form_Insert_cancelar">
-            <button
-              type="submit"
-              className="crud_Form_Insert_cancelar_button"
-              onClick={(e) => handleSubmit(e)}
-            >
-              Insert
+            <button className="crud_Form_Insert_cancelar_button" type="submit">
+              Editar
             </button>
             <button
               className="crud_Form_Insert_cancelar_button_danger"
               onClick={(e) => cerrarModalInsertar(e)}
             >
-              Return
+              Cancel
             </button>
           </div>
         </div>
@@ -256,4 +329,4 @@ console.log("data", categories)
   );
 };
 
-export default Insert;
+export default Update;
