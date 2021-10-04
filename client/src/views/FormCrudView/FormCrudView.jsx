32c4332f64a5dff47a0dd8b7188cf64171c85c99
rodriquialvo <rodriiaguantlab@gmail.com>
2021-10-04 12:42:22 -0300

    CrudV3.0

--- client/src/views/FormCrudView/FormCrudView.jsx
+++ client/src/views/FormCrudView/FormCrudView.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Insert from "./components/Insert/Insert.jsx";
-import Update from "./components/Update/UpdateProduct.jsx";
+
 
 const FormCrudView = () => {
   return (
     <div>
-      {/* <Insert /> */}
-      <Update />
+      <Insert />
+      
     </div>
   );
 };
