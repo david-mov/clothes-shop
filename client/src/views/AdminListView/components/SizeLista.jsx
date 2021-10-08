981021ff07f2a8dddeea781877d1cdcc364b51cb
Deskmont162525 <79815711+Deskmont162525@users.noreply.github.com>
2021-10-08 14:15:52 -0500

    corregido size ok

--- client/src/views/AdminListView/components/SizeLista.jsx
+++ client/src/views/AdminListView/components/SizeLista.jsx
@@ -1,8 +1,8 @@
 
-import {useDispatch,useSelector} from 'react-redux';
-import { useEffect,useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {getAllsizes} from '../../../stateManagement/actions/getAllsizes';
+import { getAllsizes } from '../../../stateManagement/actions/getAllsizes';
 import TablaList from './ListTable';
 import './styles.css'
 
@@ -15,111 +15,98 @@ export default function SizeList() {
   const [actualCurrent, setactualCurrent] = useState(1)
   var countP = 5
   var dataCompleta = [];
-   const [Input, setInput] = useState('');
-   const size = useSelector(state => state.sizesReducer.sizes);
-   const filterSizes = () => {
-     if(Input !== ''){
-       return dataCompleta = size.filter(e => e.name.toLowerCase().includes(Input.toLowerCase()));
-     }
-     return dataCompleta = size;
-   }
-   const onInputChange = (Input) =>{  
-    setInput(Input.target.value);
-}
-
-
-    const nextPage = () => {
-      if (totalCurrent !== actualCurrent) {
-        setactualCurrent(actualCurrent + 1)
-        setCurrentPage(currentPage + countP)
-      }
-
+  const [Input, setInput] = useState('');
+  const size = useSelector(state => state.sizesReducer.sizes);
+  const filterSizes = () => {
+    if (Input !== '') {
+      return dataCompleta = size.filter(e => e.name.toLowerCase().includes(Input.toLowerCase()));
     }
+    return dataCompleta = size;
+  }
+  const onInputChange = (Input) => {
+    setInput(Input.target.value);
+  }
+
+
+  const nextPage = () => {
+    if (totalCurrent !== actualCurrent) {
+      setactualCurrent(actualCurrent + 1)
+      setCurrentPage(currentPage + countP)
+    }
+
+  }
+};
+
+const prevPage = () => {
+  if (actualCurrent > 1) {
+    setactualCurrent(actualCurrent - 1);
+    setCurrentPage(currentPage - countP);
+  }
+
+  useEffect(() => {
+    dispatch(getAllsizes());
+  }, [dispatch]);
+
+  const sizes = useSelector(state => state.sizesReducer.sizes);
+  var totalCurrent = Math.ceil(sizes.length / countP)
+
+  function headers() {
+    return (
+      <thead className="table__thead">
+        <tr>
+          <th>
+            <div>
+              <input className='button'
+                type="text"
+                value={Input}
+                placeholder="search"
+                onChange={onInputChange}
+              />
+            </div>
+          </th>
+        </tr>
+        <tr>
+          <th className="table__th">Name</th>
+          <th className="table__th">Update Size</th>
+          <th className="table__th">Delete Size</th>
+        </tr>
+      </thead>
+    )
   };
 
-  const prevPage = () => {
-    if (actualCurrent > 1) {
-      setactualCurrent(actualCurrent - 1);
-      setCurrentPage(currentPage - countP);
-    }
-    
-    useEffect(() => {
-      dispatch (getAllsizes());  
-    }, [dispatch]);
-
-    const sizes = useSelector(state => state.sizesReducer.sizes);
-    var totalCurrent = Math.ceil(sizes.length / countP)
-
-    function headers (){
-        return (
-       <thead className="table__thead">
-          <tr>
-            <th>
-            <div>
-                <input className='button'
-              type = "text"
-              value = {Input}
-              placeholder = "search"
-              onChange = {onInputChange}
-              />
-           </div>
-            </th>
-          </tr>
-            <tr>                 
-                <th className="table__th">Name</th>
-                <th className="table__th">Update Size</th>
-                <th className="table__th">Delete Size</th>
-            </tr>
-          </thead>
-        )
-    };
-
-
   function bodyTable() {
-    return sizes.map((e, i) => {
-      return (
-        <tr key={i} className="table-row table-row--chris">
-          <input value={e.id} hidden />
-          <td className="table-row__td">
-            <div className="table-row__info">
-              <p className="table-row__name">{e.name}</p>
-            </div>
-          </td>
+    return filterSizes()
+      .map((e, i) => {
+        return (
+          <tr key={i} className="table-row table-row--chris">
+            <td className="table-row__td">
+              <div className="table-row__info">
+                <p className="table-row__name">{e.name}</p>
+              </div>
+            </td>
+            <td className="table-row__td">
+              <Link to={`/update/size/${e.id}`}>
+                <p>
+                  <i className="fas fa-pencil-alt  fa-2x"></i>
+                </p>
+              </Link>
+            </td>
 
-
-          filterSizes().map((e,i)=>{
-                return(
-                    <tr   key={i}  className="table-row table-row--chris">
-                    <td  className="table-row__td">
-                    <div className="table-row__info">
-                        <p className="table-row__name">{e.name}</p>
-                    
-                    </div>
-                    </td>
-                    
-                    <td  className="table-row__td">
-                    <Link to = '/'> <p><i className="fas fa-pencil-alt  fa-2x"></i></p> </Link>               
-                    </td>
-                    <td   className="table-row__td">
-                    <p><i className="fas fa-trash-alt fa-2x"></i></p>               
-                    </td>
-                    </tr>
-                )
-            }).slice(currentPage, currentPage + 5)
+          </tr>
         )
-    };
-
-    
-    
-    return (
-      <div>
-        <div className='body'>
-        <TablaList 
-            title={"SIZES"}
-            headers={headers()}
-            data={sizes}
-            bodyTable={bodyTable()}
-            url = {'/create/size'}
+      })
+      .slice(currentPage, currentPage + 5)
+  }
+  
+  return (
+    <div>
+      <div className='body'>
+        <TablaList
+          title={"SIZES"}
+          headers={headers()}
+          data={sizes}
+          bodyTable={bodyTable()}
+          url={'/create/size'}
 
         />
       </div>
--- client/src/views/NotFoundView/NotFoundView.jsx
+++ client/src/views/NotFoundView/NotFoundView.jsx
@@ -1,11 +1,11 @@
 //import NotFoundView1  from "./components/NotFoundView1";
-import NotFoundView1  from "./components/NotFoundView1";
+import NotFoundView3  from "./components/NotFoundView3";
 
 
 export default function NotFoundView() {
 	return (
 		<div>
-			<NotFoundView1 />
+			<NotFoundView3 />
 		</div>
 	)
 }
