e2c6fa1573802c0e0f56bc17ca0b7141852ac11e
David <83718324+david-mov@users.noreply.github.com>
2021-10-12 20:15:29 -0300

    app.js use cors and allow access for all client

--- api/src/app.js
+++ api/src/app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const session = require('express-session');
 const passport = require('passport');
+const cors = require('cors')
 
 require('./db.js');
 
@@ -12,12 +13,13 @@ const server = express();
 
 server.name = 'API';
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser("secretcode"));
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
