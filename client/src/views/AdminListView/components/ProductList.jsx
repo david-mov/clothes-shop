fccf9ecca0218a9467407f9f31f108cd4f7e8fe7
Agustina Ailen Aquino <ailenagustina169@gmail.com>
2021-10-13 00:01:02 -0300

    detail list y product

--- client/src/views/AdminListView/components/ProductList.jsx
+++ client/src/views/AdminListView/components/ProductList.jsx
@@ -1,52 +1,103 @@
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { getAllProducts } from "../../../stateManagement/actions/getAllProducts";
-import TablaList from "./ListTable";
-import "./styles.css";
-import Select from "react-select";
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getAllProducts } from '../../../stateManagement/actions/getAllProducts'
+import TablaList from './ListTable'
+import './styles.css'
+import Select from 'react-select'
 
 export default function ProductosLista() {
-  const dispatch = useDispatch();
-  const [currentPage, setCurrentPage] = useState(0);
-  const [actualCurrent, setactualCurrent] = useState(1);
-  var countP = 5;
-  var dataCompleta = [];
-  const [Input, setInput] = useState("");
-  const product = useSelector((state) => state.productsReducer.products);
+  const dispatch = useDispatch()
+  const [currentPage, setCurrentPage] = useState(0)
+  const [actualCurrent, setactualCurrent] = useState(1)
+  const [search, setSearch] = useState('S')
+  const [valor, setValor] = useState('')
+  var countP = 5
+  var dataCompleta = []
+  const [Input, setInput] = useState('')
+  const product = useSelector((state) => state.productsReducer.products)
   const filterProducts = () => {
-    if (Input !== "") {
-      return (dataCompleta = product.filter((e) =>
-        e.name.toLowerCase().includes(Input.toLowerCase())
-      ));
+    switch (valor) {
+      case 'input':
+        return (dataCompleta = product.filter((e) =>
+          e.name.toLowerCase().includes(Input.toLowerCase()),
+        ))
+
+      case 'Max Stock':
+        return (dataCompleta = product.sort((a,b)=>{
+          const StockA = parseInt(a.stock);
+                const StockB = parseInt(b.stock);
+                if (StockA > StockB)return -1;
+                if (StockA < StockB)return 1;
+                return 0;
+        }))
+
+      case 'Min Stock':
+        return (dataCompleta = product.sort((a,b)=>{
+          return a.stock - b.stock
+        }))
+
+      case 'Max Price':
+        return (dataCompleta = product.sort((a,b)=>{
+          const PriceA = parseInt(a.price);
+                const PriceB = parseInt(b.price);
+                if (PriceA > PriceB)return -1;
+                if (PriceA < PriceB)return 1;
+                return 0;
+        }))
+
+      case 'Min Price':
+        return (dataCompleta = product.sort((a,b)=>{
+          return a.price - b.price
+        }))
+      default:
+        return (dataCompleta = product)
     }
-    return (dataCompleta = product);
-  };
+
+    /* if (Input !== "") {
+      const stock = state.product.sort((a,b)=>b.stock - a.stock  ):
+                state.product.sort((a,b)=> a.stock- b.stock )
+      ));
+    }*/
+  }
 
   const onInputChange = (Input) => {
-    setInput(Input.target.value);
-  };
+    setValor('input')
+    setInput(Input.target.value)
+  }
+
+  const OptionSelect = [
+    { label: 'Max Stock', value: 'Max Stock' },
+    { label: 'Min Stock', value: 'Min Stock' },
+    { label: 'Max Price', value: 'Max Price' },
+    { label: 'Min Price', value: 'Min Price' },
+  ]
+
+  const handleChangeSelect = (search) => {
+    setValor(search.value)
+    setSearch(search)
+  }
 
   const nextPage = () => {
     if (totalCurrent !== actualCurrent) {
-      setactualCurrent(actualCurrent + 1);
-      setCurrentPage(currentPage + countP);
+      setactualCurrent(actualCurrent + 1)
+      setCurrentPage(currentPage + countP)
     }
-  };
+  }
 
   const prevPage = () => {
     if (actualCurrent > 1) {
-      setactualCurrent(actualCurrent - 1);
-      setCurrentPage(currentPage - countP);
+      setactualCurrent(actualCurrent - 1)
+      setCurrentPage(currentPage - countP)
     }
-  };
+  }
 
   useEffect(() => {
-    dispatch(getAllProducts());
-  }, [dispatch]);
+    dispatch(getAllProducts())
+  }, [dispatch])
 
-  const products = useSelector((state) => state.productsReducer.products);
-  var totalCurrent = Math.ceil(products.length / countP);
+  const products = useSelector((state) => state.productsReducer.products)
+  var totalCurrent = Math.ceil(products.length / countP)
   function headers() {
     return (
       <thead className="table__thead">
@@ -62,8 +113,13 @@ export default function ProductosLista() {
               />
             </div>
           </th>
-          <th className="table__th">
-            <Select className="select-style" />
+          <th>
+            <Select
+              className="selected"
+              value={search}
+              options={OptionSelect}
+              onChange={handleChangeSelect}
+            />
           </th>
         </tr>
         <tr>
@@ -76,14 +132,14 @@ export default function ProductosLista() {
           <th className="table__th">Delete Product</th>
         </tr>
       </thead>
-    );
+    )
   }
 
   function bodyTable() {
     return filterProducts()
       .map((e, i) => {
-        var status = "status--green";
-        if (e.stock <= 5) status = "status--red";
+        var status = 'status--green'
+        if (e.stock <= 5) status = 'status--red'
         return (
           <tr key={i} className="table-row table-row--chris">
             <td className="table-row__td">
@@ -130,20 +186,21 @@ export default function ProductosLista() {
               </p>
             </td>
           </tr>
-        );
+        )
       })
-      .slice(currentPage, currentPage + 5);
+      .slice(currentPage, currentPage + 5)
   }
 
+  console.log('ailyn', product)
   return (
     <div>
       <div className="body">
         <TablaList
-          title={"Products"}
+          title={'Products'}
           headers={headers()}
           data={products}
           bodyTable={bodyTable()}
-          url={"/create/product"}
+          url={'/create/product'}
         />
       </div>
       <div className="buttonList">
@@ -160,5 +217,5 @@ export default function ProductosLista() {
         </button>
       </div>
     </div>
-  );
+  )
 }
