3dbe064a9cf918eb67ce5135877d64ac1433657d
adolfo4509 <calvo330906@gmail.com>
2021-10-14 12:35:18 -0500

    cambios3

--- client/src/views/ProductView/ProductView2.jsx
+++ client/src/views/ProductView/ProductView2.jsx
@@ -22,7 +22,6 @@ export default function ProductView() {
     };
   }, [dispatch, productId]);
 
-<<<<<<< HEAD
   const product = useSelector((state) => state.productsReducer.productDetails);
   const [tengo, setTengo] = useState(false);
   const [contador, setContador] = useState(1);
@@ -32,30 +31,6 @@ export default function ProductView() {
       ? setTengo(true)
       : dispatch(getAddToCart(product));
   };
-=======
-    var nameImagen = "";
-    const rendeImages = () => {
-        if (product.images !== undefined) {
-            if (product.images.length !== 0) {
-                return product.images.map((e) => {
-                    nameImagen = "imageProduct/" + e.name;
-                    return (
-                        <div className="div_carrousel">
-                            <img src={require(`../../assets/${nameImagen}`).default}></img>
-                        </div>
-                    );
-                });
-            } else {
-                nameImagen = "products/logo JK&A.png";
-                return (
-                    <div className="div_carrousel">
-                        <img  src={require(`../../assets/${nameImagen}`).default}></img>
-                    </div>
-                );
-            }
-        }
-    };
->>>>>>> 6ecda16f8672d1cb68aad28260abae70eeab1149
 
   var nameImagen = "";
   const rendeImages = () => {
@@ -64,22 +39,16 @@ export default function ProductView() {
         return product.images.map((e) => {
           nameImagen = "imageProduct/" + e.name;
           return (
-            <div>
-              <img
-                classNameName="imgProd"
-                src={require(`../../assets/${nameImagen}`).default}
-              ></img>
+            <div className="div_carrousel">
+              <img src={require(`../../assets/${nameImagen}`).default}></img>
             </div>
           );
         });
       } else {
         nameImagen = "products/logo JK&A.png";
         return (
-          <div>
-            <img
-              classNameName="imgProd"
-              src={require(`../../assets/${nameImagen}`).default}
-            ></img>
+          <div className="div_carrousel">
+            <img src={require(`../../assets/${nameImagen}`).default}></img>
           </div>
         );
       }
@@ -171,132 +140,119 @@ export default function ProductView() {
                 </div>
               </div>
             </div>
-<<<<<<< HEAD
-          </div>
-          <div className="texto">
-            <div className="wrapper">
-              <p className="nombre">{product.name}</p>
-              <div className="precios">
-                <p className="precio">{product.price || product.price}</p>
-                {product.price ? (
-                  <p className="precioOferta">${product.price}</p>
-                ) : (
-                  ""
-                )}
-              </div>
-              <p className={`stock ${product.stock ? "" : "out"} bold`}>
-                {product.stock
-                  ? "Disponible en tienda y listo para enviar"
-                  : "Fuera de stock"}
-              </p>
-              <p className="codigo">
-                <span className="bold">Stock Product: </span>
-                {product.stock}
-              </p>
-              <p className="codigo">
-                <span className="bold">Code Product: </span>
-                {productId}
-              </p>
-              <div className="actions">
-                <div
-                  className={`component_toCartCantidad ${
-                    !product.stock ? "disabled" : ""
-                  }`}
-                >
-                  <div
-                    className={`toCartBoton menos ${
-                      contador === 1 ? "disabled" : ""
-                    }`}
-                    onClick={removeCantidad}
-                  ></div>
-                  <div className="">{contador}</div>
-                  <div
-                    className={`toCartBoton mas ${
-                      contador === product.stock ? "disabled" : ""
-                    }`}
-                    onClick={addCantidad}
-                  ></div>
-                </div>
-                <div
-                  className={`botonTextoIcono ${
-                    !product.stock ? "disabled" : ""
-                  }`}
-                >
-                  <label className="labelBoton">Add to Car</label>
-                  <div className="icono">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-                      <path d="M2 6h10l10 40h32l8-24H16"></path>
-                      <circle cx="23" cy="54" r="4"></circle>
-                      <circle cx="49" cy="54" r="4"></circle>
-                    </svg>
-                  </div>
-=======
+
             <div className="infoRapidaModal">
-                <div className="closeModal"></div>
-                <div className="modalContainer">
-                    <div className="topContent">
-                        <div className="imagenContainer zoom_section">
-                            <div className="zoom_launcher zoomWatch" title="Closed">X
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-                                    <path d="M2.002 40h22v22h-22z"></path>
-                                    <path d="M2 28V2h60v60H36"></path>
-                                    <path d="M30 34l22-22m-16 0h16v16"></path>
-                                </svg>
-                            </div>
-                            <div className="zoom_imgOrigin wrapperImg">
-                                <div id="emotes" className="zoom_imgSource imagen " >
-                                   
-                                        {rendeImages()}
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        <div className="texto">
-                            <div className="wrapper">
-                                <p className="nombre">{product.name}</p>
-                                <div className="precios">
-                                    <p className="precio">{product.price || product.price}</p>
-                                    {product.price ? <p className="precioOferta">${product.price}</p> : ''}
-                                </div>
-                                <p className={`stock ${product.stock ? '' : 'out'} bold`}>{product.stock ? 'Disponible en tienda y listo para enviar' : 'Fuera de stock'}</p>
-                                <p className="codigo"><span className="bold">Stock Product: </span>{product.stock}</p>
-                                <p className="codigo"><span className="bold">Code Product: </span>{productId}</p>
-                                <p className="descripcion"><span className="bold">Color: </span>{product.color}</p>
-                                <p className="descripcion"><span className="bold">Type: </span></p>
-                                <p className="descripcion"><span className="bold">Sizes: </span></p>
-                                <div className="actions">
-                                    <div className={`component_toCartCantidad ${!product.stock ? 'disabled' : ''}`}>
-                                        <div className={`toCartBoton menos ${contador === 1 ? 'disabled' : ''}`} onClick={removeCantidad}></div>
-                                        <div className="">{contador}</div>
-                                        <div className={`toCartBoton mas ${contador === product.stock ? 'disabled' : ''}`} onClick={addCantidad}></div>
-                                    </div>
-                                    <div className={`botonTextoIcono ${!product.stock ? 'disabled' : ''}`}>
-                                        <label className="labelBoton">Add to Car</label>
-                                        <div className="icono">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-                                                <path d="M2 6h10l10 40h32l8-24H16"></path>
-                                                <circle cx="23" cy="54" r="4"></circle>
-                                                <circle cx="49" cy="54" r="4"></circle>
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                                <p className="descripcion"><span className="bold">Description: </span></p>
-                                <p className="descripcion">{product.description}</p>
-                                <Link className="boton" to={`/product/${productId}`}>
-                                    Show Details
-                                </Link>
-                            </div>
-                        </div>
+              <div className="closeModal"></div>
+              <div className="modalContainer">
+                <div className="topContent">
+                  <div className="imagenContainer zoom_section">
+                    <div className="zoom_launcher zoomWatch" title="Closed">
+                      X
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 64 64"
+                      >
+                        <path d="M2.002 40h22v22h-22z"></path>
+                        <path d="M2 28V2h60v60H36"></path>
+                        <path d="M30 34l22-22m-16 0h16v16"></path>
+                      </svg>
                     </div>
->>>>>>> 6ecda16f8672d1cb68aad28260abae70eeab1149
+                    <div className="zoom_imgOrigin wrapperImg">
+                      <div id="emotes" className="zoom_imgSource imagen ">
+                        {rendeImages()}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="texto">
+                    <div className="wrapper">
+                      <p className="nombre">{product.name}</p>
+                      <div className="precios">
+                        <p className="precio">
+                          {product.price || product.price}
+                        </p>
+                        {product.price ? (
+                          <p className="precioOferta">${product.price}</p>
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                      <p className={`stock ${product.stock ? "" : "out"} bold`}>
+                        {product.stock
+                          ? "Disponible en tienda y listo para enviar"
+                          : "Fuera de stock"}
+                      </p>
+                      <p className="codigo">
+                        <span className="bold">Stock Product: </span>
+                        {product.stock}
+                      </p>
+                      <p className="codigo">
+                        <span className="bold">Code Product: </span>
+                        {productId}
+                      </p>
+                      <p className="descripcion">
+                        <span className="bold">Color: </span>
+                        {product.color}
+                      </p>
+                      <p className="descripcion">
+                        <span className="bold">Type: </span>
+                      </p>
+                      <p className="descripcion">
+                        <span className="bold">Sizes: </span>
+                      </p>
+                      <div className="actions">
+                        <div
+                          className={`component_toCartCantidad ${
+                            !product.stock ? "disabled" : ""
+                          }`}
+                        >
+                          <div
+                            className={`toCartBoton menos ${
+                              contador === 1 ? "disabled" : ""
+                            }`}
+                            onClick={removeCantidad}
+                          ></div>
+                          <div className="">{contador}</div>
+                          <div
+                            className={`toCartBoton mas ${
+                              contador === product.stock ? "disabled" : ""
+                            }`}
+                            onClick={addCantidad}
+                          ></div>
+                        </div>
+                        <div
+                          className={`botonTextoIcono ${
+                            !product.stock ? "disabled" : ""
+                          }`}
+                        >
+                          <label className="labelBoton">Add to Car</label>
+                          <div className="icono">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 64 64"
+                            >
+                              <path d="M2 6h10l10 40h32l8-24H16"></path>
+                              <circle cx="23" cy="54" r="4"></circle>
+                              <circle cx="49" cy="54" r="4"></circle>
+                            </svg>
+                          </div>
+                        </div>
+                      </div>
+                      <p className="descripcion">
+                        <span className="bold">Description: </span>
+                      </p>
+                      <p className="descripcion">{product.description}</p>
+                      <Link className="boton" to={`/product/${productId}`}>
+                        Show Details
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <p className="descripcion">{product.description}</p>
-              <Link className="boton" to={`/product/${productId}`}>
-                Show Details
-              </Link>
             </div>
+            <p className="descripcion">{product.description}</p>
+            <Link className="boton" to={`/product/${productId}`}>
+              Show Details
+            </Link>
           </div>
         </div>
       </div>
