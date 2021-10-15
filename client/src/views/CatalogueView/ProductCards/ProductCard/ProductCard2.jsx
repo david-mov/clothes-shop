f2cf5b56720439b4a7edd47e070ec1e67fb4e4c3
Deskmont162525 <79815711+Deskmont162525@users.noreply.github.com>
2021-10-15 14:56:02 -0500

    corregido

--- client/src/views/CatalogueView/ProductCards/ProductCard/ProductCard2.jsx
+++ client/src/views/CatalogueView/ProductCards/ProductCard/ProductCard2.jsx
@@ -63,71 +63,7 @@ function ProductCard2(props) {
       setContador(contador - 1);
     }
   }
-
-    const vistaRapidaProduct = () => {
-
-        return (
-
-            <div>
-                <div className="infoRapidaModal">
-                    <div className="closeModal"></div>
-                    <div className="modalContainer">
-                        <div className="topContent">
-                            <div className="imagenContainer zoom_section">
-                                <div className="zoom_launcher zoomWatch" title="Closed" onClick={hidenVistaRapida}>X
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-                                        <path d="M2.002 40h22v22h-22z"></path>
-                                        <path d="M2 28V2h60v60H36"></path>
-                                        <path d="M30 34l22-22m-16 0h16v16"></path>
-                                    </svg>
-                                </div>
-                                <div className="zoom_imgOrigin wrapperImg" onClick={hidenVistaRapida}>
-                                    <div className="zoom_imgSource imagen" >
-                                        <img classNameName="imgProd" src={require(`../../../../assets/${nameImagen}`).default}></img>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="texto">
-                                <div className="wrapper">
-                                    <p className="nombre">{name}</p>
-                                    <div className="precios">
-                                        <p className="precio">{price || price}</p>
-                                        {price ? <p className="precioOferta">${price}</p> : ''}
-                                    </div>
-                                    <p className={`stock ${stock ? '' : 'out'} bold`}>{stock ? 'Disponible en tienda y listo para enviar' : 'Fuera de stock'}</p>
-                                    <p className="codigo"><span className="bold">Stock Product: </span>{stock}</p>
-                                    <p className="codigo"><span className="bold">Code Product: </span>{productId}</p>
-                                    <div className="actions">
-                                        <div className={`component_toCartCantidad ${!stock ? 'disabled' : ''}`}>
-                                            <div className={`toCartBoton menos ${contador === 1 ? 'disabled' : ''}`} onClick={removeCantidad}></div>
-                                            <div className="">{contador}</div>
-                                            <div className={`toCartBoton mas ${contador === stock ? 'disabled' : ''}`} onClick={addCantidad}></div>
-                                        </div>
-                                        <div className={`botonTextoIcono ${!stock ? 'disabled' : ''}`}>
-                                            <div className="labelBoton" onClick={(ev) => addToCart(ev)}>Add to Cart</div>
-                                            <div className="icono">
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
-                                                    <path d="M2 6h10l10 40h32l8-24H16"></path>
-                                                    <circle cx="23" cy="54" r="4"></circle>
-                                                    <circle cx="49" cy="54" r="4"></circle>
-                                                </svg>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <p className="descripcion">{description}</p>
-                                    <Link className="boton" to={`/product/${productId}`}>
-                                        Show Details
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
-  
+      
   const vistaRapidaProduct = () => {
 
     return (
