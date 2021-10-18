11fe9a4fd6fbe5d269248bcd79bdfd9524bdd1c2
adolfo4509 <calvo330906@gmail.com>
2021-10-18 18:53:11 -0500

    Styles3

--- client/src/views/CatalogueView/ProductCards/ProductCards2.jsx
+++ client/src/views/CatalogueView/ProductCards/ProductCards2.jsx
@@ -7,7 +7,7 @@ import { getAllCart } from "../../../stateManagement/actions/getAllCart";
 import { useDispatch } from "react-redux";
 
 const Product = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllCart());
@@ -16,7 +16,7 @@ const Product = () => {
   var { products } = useSelector((state) => state.productsReducer);
   //aca el estado ratin
   var rating = 2;
-  //paginator 
+  //paginator
   var countP = 5;
   var totalCurrent = Math.ceil(products.length / countP);
   const [currentPage, setCurrentPage] = useState(0);
@@ -34,7 +34,7 @@ const Product = () => {
       setactualCurrent(actualCurrent - 1);
       setCurrentPage(currentPage - countP);
     }
-  };  
+  };
 
   const show = () => {
     if (actualCurrent === 1) {
@@ -43,50 +43,57 @@ const Product = () => {
           <p className="pagination-item active">{actualCurrent}</p>
           <p>TO</p>
           <p className="pagination-item ">{totalCurrent}</p>
-          <p className="pagination-item " onClick={nextPage}>next</p>
+          <p className="pagination-item " onClick={nextPage}>
+            next
+          </p>
         </div>
-      )
+      );
     } else if (actualCurrent >= totalCurrent) {
-
       return (
         <div className="pagination">
-          <p className="pagination-item " onClick={prevPage}>prev</p>
+          <p className="pagination-item " onClick={prevPage}>
+            prev
+          </p>
           <p className="pagination-item active">{actualCurrent}</p>
           <p>TO</p>
           <p className="pagination-item ">{totalCurrent}</p>
         </div>
-      )
+      );
     } else {
-
       return (
         <div className="pagination">
-          <p className="pagination-item " onClick={prevPage}>prev</p>
+          <p className="pagination-item " onClick={prevPage}>
+            prev
+          </p>
           <p className="pagination-item active">{actualCurrent}</p>
           <p>TO</p>
           <p className="pagination-item ">{totalCurrent}</p>
-          <p className="pagination-item " onClick={nextPage}>next</p>
+          <p className="pagination-item " onClick={nextPage}>
+            next
+          </p>
         </div>
-      )
+      );
     }
-  }
+  };
 
   return (
     <div>
-    {show()}
+      {show()}
       <div className="homescreen__products">
-        {products?.map((e) => (
-          <ProductCard2
-            key={e.id}
-            name={e.name}
-            price={e.price}
-            description={e.description}
-            stock={e.stock}
-            productId={e.id}
-            rating={rating}
-            image={e.images[0]}
-          />
-        )).slice(currentPage, currentPage + countP)}
-
+        {products
+          ?.map((e) => (
+            <ProductCard2
+              key={e.id}
+              name={e.name}
+              price={e.price}
+              description={e.description}
+              stock={e.stock}
+              productId={e.id}
+              rating={rating}
+              image={e.images[0]}
+            />
+          ))
+          .slice(currentPage, currentPage + countP)}
       </div>
       {show()}
     </div>
