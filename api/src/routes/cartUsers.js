7928ae30c46d5a6a5b6dc3e01f90f9d8e80bdedc
Deskmont162525 <79815711+Deskmont162525@users.noreply.github.com>
2021-10-20 12:43:15 -0500

    maldito carrito

--- api/src/routes/cartUsers.js
+++ api/src/routes/cartUsers.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 
 const postCartUsers = require('../controllers/postCartUsers');
 const getCartUsers = require('../controllers/getCartUsers');
+const getCartUsers2 = require('../controllers/getCartUsers2');
 const deleteCartUsers = require("../controllers/deleteCartUsers");
 const modifyCartUsers = require("../controllers/modifyCartUsers");
 
 router.post('/', postCartUsers);
 router.get('/', getCartUsers);
+router.get('/:Cart_Users', getCartUsers2);
 router.post('/delete_cart', deleteCartUsers);
 
 
