9d30d6a4934729f118818b22938b881a1eb87407
Francisco torres <ftorres767@gmail.com>
2021-10-20 14:14:33 -0300

    cambio modify

--- api/src/controllers/modifyProduct.js
+++ api/src/controllers/modifyProduct.js
@@ -1,12 +1,12 @@
-const { Product} = require('../db.js')
+const { Product, Type, Category} = require('../db.js')
 
 const modifyProduct = async (req, res, next) => {
 	const { id } = req.params;
+	console.log("ACA TA EL BODY",req.body)
 	try {
-		const modifiedProduct = await Product.update(req.body, 
-			{ where: { id } }
-		)
-		
+		const modifiedProduct = await Product.update(req.body, { 
+			 where: { id } 
+		})
 		res.json(modifiedProduct);
 	}
 	catch (err) {
