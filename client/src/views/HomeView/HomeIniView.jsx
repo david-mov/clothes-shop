db0ca2abe76fbb11d570ef9807edd0ce52362aa2
adolfo4509 <calvo330906@gmail.com>
2021-10-29 00:16:00 -0500

    Cambios styles

--- client/src/views/HomeView/HomeIniView.jsx
+++ client/src/views/HomeView/HomeIniView.jsx
@@ -6,28 +6,24 @@ import MenuButton from "./MenuButon";
 import NavbarHam from "./NavBarHam";
 import { HeaderWrapper } from "./Header";
 import { useEffect } from "react";
-import {getLastProducts} from "../../stateManagement/actions/getLastProducts"
+import { getLastProducts } from "../../stateManagement/actions/getLastProducts";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function HomeIniView() {
-  const products = useSelector(state => state.productsReducer.lastProducts)
+  const products = useSelector((state) => state.productsReducer.lastProducts);
   const [open, setOpen] = useState(false);
-  const  dispatch = useDispatch()
-
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getLastProducts())
-  }, [])
+    dispatch(getLastProducts());
+  }, []);
 
-  console.log("ACA TA TU ESTADO PA", products)
-
-
+  console.log("ACA TA TU ESTADO PA", products);
 
   const handleClick = () => {
     setOpen(!open);
   };
   return (
-
     <div>
       <div className="contenedor contenido-header">
         <Link to="/">
@@ -49,10 +45,9 @@ export default function HomeIniView() {
         <div className="container1">
           <div className="body_hiview">
             <ul className="c-accordion">
-              {
-                products.map((e) => {
-                  return (
-                <li id="joker" className="c-accordion__item">
+              {products.map((e) => {
+                return (
+                  <li id="joker" className="c-accordion__item">
                     <div className="c-accordion__content">
                       <h2 className="c-accordion__title c-accordion__title--hero c-accordion__title--hover-show">
                         {e.name}
@@ -61,9 +56,9 @@ export default function HomeIniView() {
                         {e.description}
                       </p>
                     </div>
-                </li>
-              )})
-              }
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
